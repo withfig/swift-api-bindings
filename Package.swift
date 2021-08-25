fbbd0fd5e2f4b8bc5bbd5be316dd6a033626d627
Matt Schrage <matt@fig.io>
2021-08-25 16:49:41 -0700

    Add swift package

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-api-bindings",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "swift-api-bindings",
+            targets: ["swift-api-bindings"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.17.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "swift-api-bindings",
+            dependencies: ["SwiftProtobuf" ]),
+    ]
+)
