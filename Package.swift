dcac957dc97da5ef8b4e55f991f79909ad490b6c
Matt Schrage <matthewschrage@gmail.com>
2021-08-25 17:37:01 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-api-bindings",
+    name: "FigAPIBindings",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "swift-api-bindings",
+            name: "FigAPIBindings",
             targets: ["swift-api-bindings"]),
     ],
     dependencies: [
